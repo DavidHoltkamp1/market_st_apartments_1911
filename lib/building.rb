aa34c4f323382a1ce4256bf0a7b24eda4e7f1d00
David Holtkamp <david.hholtkamp@gmail.com>
2019-12-11 10:29:47 -0700

    Write functionality to determine renter with highest rent.. booyaah

--- lib/building.rb
+++ lib/building.rb
@@ -15,4 +15,14 @@ attr_reader :units
     end
     average =  (total.sum.to_f / @units.count).round(1)
   end
+
+  def renter_with_highest_rent
+    rented_units = @units.find_all do |unit|
+      unit.renter != nil
+    end
+    highest_rent = rented_units.max_by do |unit|
+      unit.monthly_rent
+    end
+    highest_rent.renter
+  end
 end
